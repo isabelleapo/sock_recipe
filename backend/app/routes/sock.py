9ffa8accf6f0d1db1d59c062c1112507da2c8cfc
Isabelle <isabelle.hapostolopoulos@gmail.com>
2024-12-15 11:15:22 +0000

    api working functions working

--- backend/app/routes/sock.py
+++ backend/app/routes/sock.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from core import Sock
+from fastapi.responses import PlainTextResponse
+from app.core.sock import Sock
 from typing import Tuple
 
 
@@ -7,23 +8,32 @@ def create_sock_router() -> APIRouter:
     sock_router = APIRouter()
 
     @sock_router.get("/heel-start-length/")
-    async def get_heel_start_length(
+    def get_heel_start_length(
         row_gauge: int | float,
         foot_length: int | float,
-        heel_stitch_sections: Tuple[int],
+        size: str, 
+        yarn_weight: str,
     ) -> float:
-        side_heel_section, _, _ = heel_stitch_sections
+        sock = Sock(size=size, yarn_weight=yarn_weight)
+        side_heel_section, _, _ = sock.heel_stitch_sections
         no_heel_rows = side_heel_section * 2
         heel_length = no_heel_rows / row_gauge
         heel_start_length = foot_length - heel_length
         return heel_start_length
 
     @sock_router.get("/heel-stitch-sections/")
-    async def get_heel_stitch_sections(size: int, yarn_weight: str) -> Tuple[int]:
+    def get_heel_stitch_sections(size: str, yarn_weight: str) -> Tuple[int, int, int]:
         """
         Construct the heel layout.
         """
         sock = Sock(size=size, yarn_weight=yarn_weight)
         return sock.heel_stitch_sections
+    @sock_router.get("/full-pattern/")
+    def get_full_pattern(size: str, yarn_weight: str) -> PlainTextResponse:
+        """
+        Get full pattern for sock pattern
+        """
+        sock = Sock(size=size, yarn_weight=yarn_weight)
+        return sock.full_pattern
 
     return sock_router
